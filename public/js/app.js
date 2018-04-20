d126efcb48bc01ef28f789082487694cc989f88a
graemeerickson <graemeerickson@gmail.com>
2018-04-19 22:07:16 -0700

    Moved all rendering functionality to front-end app.js file. New posts now add instantly to the DOM.

--- public/js/app.js
+++ public/js/app.js
@@ -10,13 +10,16 @@
  * 5. Perform the deletion in the database
  *
  */
+var $albumList;
+var allAlbums = [];
 
 $(document).ready(function() {
-  console.log('app.js loaded!');
+  $albumList = $('#albumTarget');
+
   $.ajax({
     method: 'GET',
     url: '/api/albums',
-    // success: handleSuccess,
+    success: handleSuccess
     // error: handleError
   });
 
@@ -25,13 +28,75 @@ $(document).ready(function() {
     $.ajax({
       method: 'POST',
       url: '/api/albums',
-      data: $(this).serialize()
-      // success: newAlbumSuccess,
+      data: $(this).serialize(),
+      success: newAlbumSuccess
       // error: handleError
     })
-    $('#name').val('');
-    $('#textinput').val('');
-    $('#releaseDate').val('');
-    $('#genres').val('');
   });
 });
+
+function getAlbumHtml(album) {
+  return `<!-- one album -->
+          <div class="row album">
+            <div class="col-md-10 col-md-offset-1">
+              <div class="panel panel-default">
+                <div class="panel-body">
+                <!-- begin album internal row -->
+                  <div class='row'>
+                    <div class="col-md-3 col-xs-12 thumbnail album-art">
+                      <img src="http://placehold.it/800x800" alt="album image">
+                    </div>
+                    <div class="col-md-9 col-xs-12">
+                      <ul class="list-group">
+                        <li class="list-group-item">
+                          <h4 class='inline-header'>Album Name:</h4>
+                          <span class='album-name'>${album.name}</span>
+                        </li>
+                        <li class="list-group-item">
+                          <h4 class='inline-header'>Artist Name:</h4>
+                          <span class='artist-name'>${album.artistName}</span>
+                        </li>
+                        <li class="list-group-item">
+                          <h4 class='inline-header'>Released date:</h4>
+                          <span class='album-releaseDate'>${album.releaseDate}</span>
+                        </li>
+                      </ul>
+                    </div>
+                  </div>
+                  <!-- end of album internal row -->
+                  <div class='panel-footer'>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <!-- end one album -->`;
+}
+
+function getAllAlbumsHtml(albums) {
+  return albums.map(getAlbumHtml).join("");
+}
+
+// helper function to render all albums to view
+// note: we empty and re-render the collection each time our post data changes
+function render () {
+  // empty existing albums from view
+  $albumList.empty();
+
+  // pass `allTodos` into the template function
+  let albumHtml = getAllAlbumsHtml(allAlbums);
+
+  // append html to the view
+  $albumList.append(albumHtml);
+};
+
+function handleSuccess(json) {
+  allAlbums = json;
+  render();
+}
+
+function newAlbumSuccess (json) {
+  $('#newAlbumForm input').val('');
+  allAlbums.push(json);
+  render();
+}
--- server.js
+++ server.js
@@ -41,17 +41,18 @@ Then, take a look into the seed.js file to populate some starter data.
 app.get('/', function homepage (req, res) {
   // This albums variable is the array of objects defined above.
   // TODO: Eventually, this should be replaced with a find() call to your database!
-  db.Album.find(function (err, albums){
-    res.render('index', { albums: albums });
-  });
+  // db.Album.find(function (err, albums){
+    res.render('index');
+  // });
 });
 
 app.get('/api/albums', function (req, res) {
   db.Album.find(function (err, albums){
+    console.log('app.get(/api/albums)');
+    console.log('albums:',albums);
     res.json(albums);
   });
 });
-
 
 // TODO: GET ROUTE for single album (Route has an id in the url. e.g., /:id that can be accessed
 // on the request object with req.params.id).
@@ -66,9 +67,8 @@ app.post('/api/albums', function (req, res) {
   }
   let newAlbum = new db.Album({name:req.body.name, artistName:req.body.artistName, releaseDate:req.body.releaseDate, genres:genreArr})
   newAlbum.save()
-  res.json('/api/albums');
+  res.json(newAlbum);
 });
-
 
 /*
  * JSON API Endpoints: This usually means AJAX has been used to call this route
