9c08f0460579d3c21ef46e2dc22e9fa31c8f6170
smithalex24 <smith25243@gmail.com>
2018-04-20 08:46:45 -0700

    completed Step 1 of Sprint 3

--- models/album.js
+++ models/album.js
@@ -1,12 +1,14 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
+const songs = require('./song.js');
 
 const AlbumSchema = new Schema({
   artistName: String,
   name: String,
   releaseDate: String,
   genres: [ String ],
-  albumArt: String
+  albumArt: String,
+  songs: [ String ]
 });
 
 const Album = mongoose.model('Album',AlbumSchema);
